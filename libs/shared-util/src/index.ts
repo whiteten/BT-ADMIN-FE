1346c4440ebeb7a5a9eaf769300509a8cbedfb71
이호재 <hojaee@bridgetec.co.kr>
2025-12-05 13:49:52 +0900

    ✨feat: toast customize용 wrapper 생성

--- libs/shared-util/src/index.ts
+++ libs/shared-util/src/index.ts
@@ -3,3 +3,4 @@ export { default } from './lib/apiClient';
 export type { ApiClientOptions } from './lib/apiClient';
 export { default as WebSocketClient } from './lib/webSocketClient';
 export type * from './lib/types/query.types';
+export { toast } from './lib/toast';
