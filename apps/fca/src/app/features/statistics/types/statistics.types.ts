3530c39233e4c659330a73e6893193e3fd3f11d6
원선영 <won5972@bridgetec.co.kr>
2026-04-13 14:06:31 +0900

    🔥remove(fca): 슬롯 통계 서비스 플로우 이동 버튼 삭제

--- apps/fca/src/app/features/statistics/types/statistics.types.ts
+++ apps/fca/src/app/features/statistics/types/statistics.types.ts
@@ -33,13 +33,8 @@ export interface SlotStatItem {
   tenantId?: number; // 테넌트 ID
   serviceId?: string; // 봇서비스 ID
   serviceName: string; // 봇서비스명
-  serviceVer: string; // 봇서비스 버전
   dialogId: string; // 대화 ID
   dialogName: string; // 대화명
-  prevDialogId: number; // 이전 대화 ID
-  prevDialogName: string; // 이전 대화명
-  fnId: string; // IFE SubFlow ID
-  fnName: string; // IFE SubFlow명
   slotId: string; // 슬롯 ID
   slotName: string; // 슬롯명
   isCustomSlot: number; // 슬롯타입
